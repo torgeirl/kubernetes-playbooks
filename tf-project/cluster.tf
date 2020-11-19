a74056c7f9da756eece35d3eec78e29e6bfb1fcd
Torgeir Lebesbye <torgeir@lebesbye.com>
2020-11-19 18:08:47 +0100

    user configurations are now loaded from environment variables (#9)

--- tf-project/cluster.tf
+++ tf-project/cluster.tf
@@ -1,11 +1,12 @@
 # build instances on Openstack for a Kubernetes cluster
-locals {
-  worker_count = 19
-  image_name = "GOLD Ubuntu 20.04 LTS"
-  key_pair = "k8s-nodes"
-  key_pair_location = "~/.ssh"
-  ssh_security_group = "SSH and ICMP"
-}
+variable "K8S_WORKER_COUNT" {}
+variable "K8S_WORKER_FLAVOR" {}
+variable "K8S_IMAGE_NAME" {}
+variable "K8S_NETWORK_NAME" {}
+variable "K8S_NETWORK_VERSION" {}
+variable "K8S_KEY_PAIR" {}
+variable "K8S_KEY_PAIR_LOCATION" {}
+variable "K8S_SECURITY_GROUP" {}
 
 provider "openstack" {}
 
@@ -40,38 +41,39 @@ resource "openstack_networking_secgroup_rule_v2" "rule_k8s_tcp_6443_ipv6" {
 resource "openstack_compute_instance_v2" "master_instance" {
   count = 1 
   name = "k8s-master-${count.index+1}"
-  image_name = local.image_name
-  flavor_name = "${local.worker_count < 11 ? "m1.large" : "m1.xlarge"}"
+  image_name = var.K8S_IMAGE_NAME
+  flavor_name = var.K8S_WORKER_COUNT < 11 ? "m1.large" : "m1.xlarge"
 
-  key_pair = local.key_pair
-  security_groups = [ local.ssh_security_group, openstack_networking_secgroup_v2.instance_comms.name ]
+  key_pair = var.K8S_KEY_PAIR
+  security_groups = [ var.K8S_SECURITY_GROUP, openstack_networking_secgroup_v2.instance_comms.name ]
 
   network {
-    name = "dualStack"
+    name = var.K8S_NETWORK_NAME
   }
 }
 
 resource "openstack_compute_instance_v2" "worker_instance" {
-  count = local.worker_count
+  count = var.K8S_WORKER_COUNT
   name = "k8s-worker-${count.index+1}"
-  image_name = local.image_name
-  flavor_name = "c1.large"
+  image_name = var.K8S_IMAGE_NAME
 
-  key_pair = local.key_pair
-  security_groups = [ local.ssh_security_group, openstack_networking_secgroup_v2.instance_comms.name ]
+  flavor_name = var.K8S_WORKER_FLAVOR
+
+  key_pair = var.K8S_KEY_PAIR
+  security_groups = [ var.K8S_SECURITY_GROUP, openstack_networking_secgroup_v2.instance_comms.name ]
 
   network {
-    name = "dualStack"
+    name = var.K8S_NETWORK_NAME
   }
 }
 
 resource "local_file" "ansible_inventory" {
-  content = "[master]\n${openstack_compute_instance_v2.master_instance[0].name} ansible_host=${openstack_compute_instance_v2.master_instance[0].access_ip_v4}\n\n[workers]\n${join("\n",
+  content = "[master]\n${openstack_compute_instance_v2.master_instance[0].name} ansible_host=var.K8S_NETWORK_VERSION > 4 ? openstack_compute_instance_v2.master_instance[0].access_ip_v6 : openstack_compute_instance_v2.master_instance[0].access_ip_v4\n\n[workers]\n${join("\n",
              formatlist(
                "%s ansible_host=%s",
                openstack_compute_instance_v2.worker_instance.*.name,
-               openstack_compute_instance_v2.worker_instance.*.access_ip_v4
-             ))}\n\n[all:vars]\nansible_python_interpreter=/usr/bin/python3\nansible_ssh_extra_args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'\nansible_ssh_private_key_file=${local.key_pair_location}/${local.key_pair}\nansible_user=ubuntu"
+               var.K8S_NETWORK_VERSION > 4 ? openstack_compute_instance_v2.worker_instance.*.access_ip_v6 : openstack_compute_instance_v2.worker_instance.*.access_ip_v4
+             ))}\n\n[all:vars]\nansible_python_interpreter=/usr/bin/python3\nansible_ssh_extra_args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'\nansible_ssh_private_key_file=${var.K8S_KEY_PAIR_LOCATION}/${var.K8S_KEY_PAIR}\nansible_user=ubuntu"
 
   file_permission = "0600"
   filename = "ansible_inventory"
